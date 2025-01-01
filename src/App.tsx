f7b235425ee44fe5dfe5eb06a376939dce41a5f4
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-01-02 02:33:47 +0600

    implement counter functionality with Redux for increment and decrement actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { useState } from 'react';
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/features/counter/counterSlice';
 
 function App() {
-    const [count, setCount] = useState(0)
+    const dispatch = useDispatch();
+    const  count  = useSelector((state : number) => state.count);
+    console.log('count', count);
+
+    const handelIncrement = () => {
+        dispatch(increment());
+    };
+    const handelDecrement = () => {
+        dispatch(decrement());
+    };
 
     return (
         <>
             <h1>Learning Redux</h1>
             <div className="card">
                 <h2>count is {count}</h2>
-                <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+
+                <button onClick={handelIncrement} >Increment</button>
+                <button onClick={handelDecrement}>Decrement</button>
             </div>
         </>
     )
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,13 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: 0
+    count: 0
 };
 
 const counterSlice = createSlice({
     name: "counter",
     initialState,
-    reducers: {},
+    reducers: {
+        increment: (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        },
+    },
 });
 
+export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
